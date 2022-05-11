d0368721a4d9b2e8914ec8ae1459e8f0fa866a3c
suny <me@suny.cloud>
2022-05-11 17:27:33 +0800

    wezterm: set default font size and MacOS native fullscreen to true

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,13 +2,18 @@ local wezterm = require "wezterm"
 
 return {
   -- Spawn a fish shell in login mode
-  default_prog = {"/usr/local/bin/fish", "-l"},
+  default_prog = { "/usr/local/bin/fish", "-l" },
+
+  -- Specifies whether the ToggleFullScreen key assignment
+  -- uses the native macOS full-screen application support or not.
+  native_macos_fullscreen_mode = true,
 
   -- Set default font
-  -- font = wezterm.font({
-  --   family="JetBrains Mono",
-  --   harfbuzz_features={"calt=0", "clig=0", "liga=0"}
-  -- })
+  font_size = 12,
+  font = wezterm.font({
+    family = "JetBrains Mono",
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  }),
 
   -- Set color scheme
   color_scheme = "Dark Pastel",
